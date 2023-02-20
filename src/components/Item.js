dd0c95bd5a1458db8127121d3f2ef79290b1553c
vikas kumar <vk.vikas3003@gmail.com>
2023-02-20 16:56:57 +0530

    additem n removeitem actions added

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import styles from './Item.module.css'
+import styles from './Item.module.css';
+import {useDispatch,useSelector} from 'react-redux'
+import { cartActions } from '../store/cart-slice';
 
 const Item = (props) => {
+    const dispatch = useDispatch();
+
+    const addToCartHandler = () => {
+        dispatch(cartActions.addItem({
+            title: props.title,
+            price:props.price,
+            id:props.id
+        }))
+    }
   return (
     <div className={styles.item}>
         <h3>{props.title}</h3>
         <p>{props.description}</p>
         <p>{props.price}</p>
-        <button>Add to cart</button>
+        <button onClick={addToCartHandler}>Add to cart</button>
     </div>
   )
 }
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -2,7 +2,7 @@ import {createSlice} from '@reduxjs/toolkit'
 
 const initialState = {
     items: [],
-    totalQuantity: 99
+    totalQuantity: 0
 
 }
 
@@ -10,8 +10,38 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        addItem(state,action) {},
-        removeItem(state,action) {}
+        addItem(state,action) {
+            state.totalQuantity++;
+            const newItem = action.payload;
+            const exisitingItem = state.items.find(i => i.id===newItem.id);
+            if(exisitingItem){
+                // if item was there just increase quantity
+                exisitingItem.quantity++;
+                exisitingItem.totalPrice += exisitingItem.price;
+            }else {
+                // a new item is being added
+                state.items.push({
+                    id:newItem.id,
+                    title:newItem.title,
+                    price:newItem.price,
+                    quantity:1,
+                    totalPrice: newItem.price
+                })
+            }
+
+        },
+        removeItem(state,action) {
+            state.totalQuantity--;
+            const newId = action.payload;
+            const exisitingItem = state.items.find(i => i.id=newId);
+            if(exisitingItem.quantity===1){
+                // last item ,must be removed
+                state.items = state.items.filter(i => i.id!==newId);
+            }else{
+                // just reduce the quantity
+                exisitingItem.quantity--;
+            }
+        }
     }
 })
 
